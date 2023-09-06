58cf44365aeed9a06e5f82e612c8ade7ba5d0476
Paul <twinencrypt@gmail.com>
2023-09-06 11:59:55 +0700

    Change host and port

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2ea5z-+!qpv5rsp6)a=s+vql)&-uzui%0^1=q&q6e0g#^9#d=%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
